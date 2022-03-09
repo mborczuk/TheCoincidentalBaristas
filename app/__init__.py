87c6e1be8b18d6a7dc1658bd08d4ea65d8eac4f6
Michael Borczuk <michaelborczuk@gmail.com>
2022-03-08 19:48:28 -0500

    comment out stuff for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template #Q0: What happens if you remove render_template from this line?
+from flask import Flask, Blueprint, request, session, render_template, redirect, g
+import os, sqlite3, json, urllib
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
-@app.route("/my_foist_template") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
+@app.route("/my_foist_template") 
 def test_tmplt():
-    return render_template('game.html') #Q2: What is the significance of each argument?
-
+    return render_template('game.html')
+'''
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='static')
     # Configure app key & DB location
@@ -27,8 +29,8 @@ def create_app():
 app = create_app()
 
 # Connect Authentication Blueprint
-app.register_blueprint(auth.bp)
-
+# app.register_blueprint(auth.bp)
+'''
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0')
+    app.debug = True;
+    app.run();
