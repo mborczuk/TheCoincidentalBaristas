8937d2c34cacc09624e374e2cd596f8c9cf11b5e
sookiemonster <sookiemonster216@gmail.com>
2022-03-12 21:01:48 -0500

    Create landing page layout. Added landing marquee and airplane bop animations.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, request, session, render_template, redirect, url_for, g
 import os, sqlite3, json, urllib
 
-import auth
+import auth, db_builder
 
 def create_app():
     app = Flask(__name__, static_url_path='', static_folder='static')
